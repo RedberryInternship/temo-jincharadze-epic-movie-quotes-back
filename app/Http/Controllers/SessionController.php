8f7e9a2001adf2edc5fa6034e53281fdabb311f1
temito93 <temotemo161@gmail.com>
2023-01-04 03:34:37 +0400

    feat: add register functionallity

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Session\RegisterUserRequest;
+use App\Mail\VerificationMail;
+use App\Models\Email;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
+
+class SessionController extends Controller
+{
+	public function register(RegisterUserRequest $request)
+	{
+		$formFields = $request->validated();
+
+		$formFields['password'] = bcrypt($formFields['password']);
+
+		$userData = $request->except(['email']);
+		$email = $request->email;
+
+		$user = User::create($userData);
+		if ($user)
+		{
+			$userEmail = Email::create([
+				'user_id' => $user->id,
+				'email'   => $email,
+			]);
+		}
+
+		$response = [
+			'user'  => $user,
+			'email' => $userEmail,
+		];
+
+		$url = URL::temporarySignedRoute('verification.verify', now()->addMinutes(30), ['id' => $user->id, 'token' => hash('sha256', $email)]);
+
+		Mail::to($email)->send(new VerificationMail(['url'=> $url, 'token' => hash('sha256', $email), 'id' => $user->id]));
+
+		return response($response, 201);
+	}
+
+	public function verify(Request $request)
+	{
+		if ($request->hasValidSignature())
+		{
+			return response('Verified', 200);
+		}
+		return response('Route expired');
+	}
+}
