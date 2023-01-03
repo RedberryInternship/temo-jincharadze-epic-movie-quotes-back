178856357f297202f9d0e38ad56d3d8ca15c8e37
temito93 <temotemo161@gmail.com>
2023-01-04 03:32:46 +0400

    chore: add Mail class for verification

---
+++ app/Mail/VerificationMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerificationMail extends Mailable
+{
+	use Queueable, SerializesModels;
+
+	public $url;
+
+	public $token;
+
+	public $id;
+
+	public function __construct($data)
+	{
+		$this->url = $data['url'];
+		$this->token = $data['token'];
+		$this->id = $data['id'];
+	}
+
+	public function envelope()
+	{
+		return new Envelope(
+			subject: 'Please verify your email address',
+		);
+	}
+
+	public function content()
+	{
+		return new Content(
+			view: 'email.emailVerification',
+		);
+	}
+}
