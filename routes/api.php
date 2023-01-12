a6fb8d4a0b95d5ac4d8581b880abe9da7ce9da60
temito93 <temotemo161@gmail.com>
2023-01-12 14:23:48 +0400

    chore: install socialite

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 //Public routes
 Route::prefix('/')->middleware('guest')->group(function () {
-	Route::post('/login', [SessionController::class, 'login']);
+	Route::post('login', [SessionController::class, 'login']);
 	Route::post('register', [SessionController::class, 'register'])->name('register');
-	Route::get('/verify-account', [SessionController::class, 'verify'])->name('verification.verify');
+	Route::get('verify-account', [SessionController::class, 'verify'])->name('verification.verify');
 	Route::post('reset-password', [PasswordController::class, 'send'])->name('reset.password');
 	Route::get('reset-check', [PasswordController::class, 'check'])->name('check');
 	Route::post('update-password', [PasswordController::class, 'update'])->name('update.password');
