579bf1c7f8826016574474a40bbac2b856f698ad
temito93 <temotemo161@gmail.com>
2023-01-23 00:16:26 +0400

    feat: add routes for movie and tag

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\SocialRegisterController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 //Public routes
-Route::prefix('/')->group(function () {
+Route::prefix('/')->middleware('guest')->group(function () {
 	Route::post('login', [SessionController::class, 'login']);
 	Route::post('register', [SessionController::class, 'register'])->name('register');
 	Route::get('verify-account', [SessionController::class, 'verify'])->name('verification.verify');
@@ -15,4 +17,13 @@ Route::prefix('/')->group(function () {
 	Route::post('update-password', [PasswordController::class, 'update'])->name('update.password');
 	Route::get('google-register/{locale}/{type}', [SocialRegisterController::class, 'redirectToProvider'])->name('google.register');
 	Route::get('auth/google/{locale}/{type}/callback', [SocialRegisterController::class, 'handleCallBack'])->name('google.register.callback');
+});
+//Auth routes
+Route::prefix('/auth')->middleware(['auth:sanctum'])->group(function () {
+	Route::get('/user', [UserController::class, 'me']);
+	Route::get('/movie-tags', [MovieController::class, 'tags']);
+	Route::post('/movie-upload', [MovieController::class, 'store']);
+	Route::get('/user-movies', [MovieController::class, 'userMovies']);
+	Route::get('/movie/{id}', [MovieController::class, 'userMovie']);
+	Route::put('/movie/{id}', [MovieController::class, 'update']);
 });
