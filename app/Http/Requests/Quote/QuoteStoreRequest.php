4d5c7fedcd081a23b556943348997eca3d477acc
temito93 <temotemo161@gmail.com>
2023-01-24 14:49:32 +0400

    chore: add custom request for quote upload

---
+++ app/Http/Requests/Quote/QuoteStoreRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\Quote;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class QuoteStoreRequest extends FormRequest
+{
+	public function rules()
+	{
+		return [
+			'movie_id' => ['required'],
+			'quoteEn'  => ['required', 'regex:/^[a-zA-Z0-9]*$/'],
+			'quoteKa'  => ['required', 'regex:/^[ა-ჰ0-9]*$/'],
+			'image'    => ['required'],
+		];
+	}
+}
