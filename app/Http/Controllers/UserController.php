ab15f84292298d92d876ea300f4ae3e0ac273eab
temito93 <temotemo161@gmail.com>
2023-01-27 03:59:30 +0400

    fix: get auth user  with correct id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
 	public function me(): JsonResponse
 	{
-		$user = auth()->user();
-		$user_data = $user->select(['id', 'name', 'image'])->first();
+		$user = User::where('id', auth()->user()->id)->select(['id', 'name', 'image'])->first();
 		$email = $user->email()->select('email', 'primary', 'id', 'user_id')->get();
 
-		return response()->json(['user' => $user_data, 'emails' => $email], 200);
+		return response()->json(['user' => $user, 'emails' => $email], 200);
 	}
 }
