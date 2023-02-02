830b277681866396f60cae3c8712f08a062f1c83
temito93 <temotemo161@gmail.com>
2023-02-02 16:01:55 +0400

    feat: add Notifications relation to quote and sender

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -10,4 +10,19 @@ class Notification extends Model
 	use HasFactory;
 
 	protected $fillable = ['user_id', 'quote_id', 'has_new', 'has_comment', 'sender_id'];
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function sender()
+	{
+		return $this->belongsTo(User::class, 'sender_id', 'id');
+	}
+
+	public function quote()
+	{
+		return $this->belongsTo(Quote::class, 'quote_id', 'id');
+	}
 }
