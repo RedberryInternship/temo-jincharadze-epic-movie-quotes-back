16d1ff6454d852fdd1d9dfc988a8df71402fa0a5
temito93 <temotemo161@gmail.com>
2023-02-02 18:18:26 +0400

    chore: install pusher

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Comment\CommentStoreRequest;
 use App\Models\Comment;
+use App\Models\Movie;
+use App\Models\Notification;
 use Illuminate\Http\JsonResponse;
 
 class CommentController extends Controller
@@ -22,6 +24,19 @@ class CommentController extends Controller
 			'quote_id' => $validated['quote_id'],
 			'comment'  => $validated['comment'],
 		]);
+
+		$movie = Movie::where('id', request('movie_id'))->first();
+
+		if ($movie->user_id !== auth()->user()->id)
+		{
+			Notification::create([
+				'user_id'     => $movie->user_id,
+				'quote_id'    => $validated['quote_id'],
+				'has_new'     => true,
+				'has_comment' => true,
+				'sender_id'   => $validated['user_id'],
+			]);
+		}
 
 		return response()->json($comment, 201);
 	}
