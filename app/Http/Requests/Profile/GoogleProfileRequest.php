b14989dfc4e09c711740cd9489a0975da8e60645
temito93 <temotemo161@gmail.com>
2023-02-07 00:39:37 +0400

    chore: add custom request for google profile

---
+++ app/Http/Requests/Profile/GoogleProfileRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests\Profile;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GoogleProfileRequest extends FormRequest
+{
+	public function rules()
+	{
+		return [
+			'username' => ['required', 'min:3', 'max:15', 'regex:/^[a-z0-9]*$/'],
+			'userId'   => ['required'],
+			'image'    => ['required'],
+		];
+	}
+}
