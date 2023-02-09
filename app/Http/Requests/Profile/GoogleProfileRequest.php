3bf4373dac56cdf87e072b933e387fe29fcfa5ec
temito93 <temotemo161@gmail.com>
2023-02-10 02:06:27 +0400

    feat: add custom request for google profile

--- app/Http/Requests/Profile/GoogleProfileRequest.php
+++ app/Http/Requests/Profile/GoogleProfileRequest.php
@@ -9,9 +9,9 @@ class GoogleProfileRequest extends FormRequest
 	public function rules()
 	{
 		return [
-			'username' => ['required', 'min:3', 'max:15', 'regex:/^[a-z0-9]*$/'],
-			'userId'   => ['required'],
-			'image'    => ['required'],
+			'username'         => ['required', 'min:3', 'max:15', 'regex:/^[a-z0-9]*$/'],
+			'userId'           => ['required'],
+			'image'            => ['required'],
 		];
 	}
 }
